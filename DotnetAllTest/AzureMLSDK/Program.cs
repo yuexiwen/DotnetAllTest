871a22991f33f2b5d982da905e57ae1b91181b27
Bobby bob <bob@gmail.com>
2024-04-15 16:25:03 +0800

    2024-04-15 change AzureMLSDK test resource group

--- DotnetAllTest/AzureMLSDK/Program.cs
+++ DotnetAllTest/AzureMLSDK/Program.cs
@@ -3,16 +3,16 @@
 Console.WriteLine("Hello, World!");
 
 const string SUBSCRIPTION_ID = "0c530e83-2bf4-40f0-902f-f897f0e2cd7e";
-const string RESOURCE_NAME = "aml-dev-dev01";
-const string AML_WORKSPACE_NAME = "aml-ml-poc-dev01";
+const string RESOURCE_NAME = "aml1-dev-dev01";
+const string AML_WORKSPACE_NAME = "aml1-ml-poc-dev01";
 const string ENVIRONMENT_NAME = "envpoc";
 const string ENVIRONMENT_VERSION = "1";
-const string CODE_URI = "https://amlsamlstodev01.blob.core.windows.net/mltest-container/";
-const string CODE_VERSION = "2";
+const string CODE_URI = "https://aml1samlstodev01.blob.core.windows.net/training-code/";
+const string CODE_VERSION = "1";
 const string CODE_ASSET_NAME = "python_code";
 const string COMPUTE_NAME = "computepoc";
 const string COMMAND = "python Training2.py";
-const string JOB_NAME = "xiwentestjob-2024-0414";
+const string JOB_NAME = "xiwentestjob-2024-0415-1603";
 
 AMLJobOperator aMLJobOperator = new();
 aMLJobOperator.subscriptionId = SUBSCRIPTION_ID;
@@ -26,7 +26,7 @@ aMLJobOperator.codeAssetName = CODE_ASSET_NAME;
 aMLJobOperator.computeName = COMPUTE_NAME;
 aMLJobOperator.command = COMMAND;
 
-await aMLJobOperator.TriggerAMLJobByNameAsync(JOB_NAME);
+//await aMLJobOperator.TriggerAMLJobByNameAsync(JOB_NAME);
 
 var jobStatus = aMLJobOperator.CheckAMLJobStatus(JOB_NAME);
 Console.WriteLine($"job xiwentestjob status = {jobStatus}");
