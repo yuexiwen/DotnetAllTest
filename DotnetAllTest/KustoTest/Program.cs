1968d8b925b1db5f6e41dc661cabed17d549b195
Xiwen Yue <xiwyue@microsoft.com>
2024-04-25 23:30:40 +0800

    2024-11-30 add kusto test

--- DotnetAllTest/KustoTest/Program.cs
+++ DotnetAllTest/KustoTest/Program.cs
@@ -7,14 +7,19 @@ Console.WriteLine("Hello, World!");
 var clusterUri = "https://cpccradxingestint01.eastus.kusto.windows.net";
 var databaseName = "Report";
 var query = "UpdatePolicy_autodenormalizationcomwrksdeviceuser_workspaceentity";
+var query1 = ".show functions";
 var kcsb = new KustoConnectionStringBuilder(clusterUri).WithAadUserPromptAuthentication();
 var kustoclient = KustoClientFactory.CreateCslQueryProvider(kcsb);
 
-using (var response = kustoclient.ExecuteQuery(databaseName, query, null))
+
+using (var response = kustoclient.ExecuteQuery(databaseName, query1, null))
 {
     while (response.Read())
     {
-        int columnNo = response.GetOrdinal("WorkspaceName");
-        Console.WriteLine(response.GetString(columnNo));
+        //int columnNo = response.GetOrdinal("WorkspaceName");
+        //Console.WriteLine(response.GetString(columnNo));
+        var functionName = response.GetString(0);
+        Console.WriteLine(functionName);
     }
 }
+
