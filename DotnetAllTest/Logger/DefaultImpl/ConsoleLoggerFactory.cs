ac5a4d7106b9da6215367e63e927662462f97e1e
Xiwen Yue <xiwyue@microsoft.com>
2024-05-01 21:38:52 +0800

    2024-05-01 add trace log

--- DotnetAllTest/Logger/DefaultImpl/ConsoleLoggerFactory.cs
+++ DotnetAllTest/Logger/DefaultImpl/ConsoleLoggerFactory.cs
@@ -1,6 +1,28 @@
 ﻿namespace Logger.DefaultImpl
 {
-    public class ConsoleLoggerFactory
+    using Logger.Interfaces;
+    using System.Collections.Generic;
+
+    public class ConsoleLoggerFactory : ILoggerFactory
     {
+        public IMeasureMetric CreateMetric(string metricNamespace, string metricName, List<string> dimensionNames)
+        {
+            return new ConsoleMetric();
+        }
+
+        public IMeasureMetric CreateMetric(string metricNamespace, string metricName, params string[] dimensionNames)
+        {
+            return new ConsoleMetric();
+        }
+
+        public ILogger GetLogger()
+        {
+            return new ConsoleLogger();
+        }
+
+        public ILogger GetLogger(string activityId)
+        {
+            return new ConsoleLogger(activityId);
+        }
     }
 }
--- DotnetAllTest/Logger/Program.cs
+++ DotnetAllTest/Logger/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Logger;
+using Logger.DefaultImpl;
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine($"frequency = {StopWatchTimer.queryPerformanceFrequency}");
+//Console.WriteLine($"sysClock = {StopWatchTimer.GetCurrent()}");
+//long startTime = StopWatchTimer.GetCurrent();
+//Thread.Sleep(997);
+//Console.WriteLine($"time = {StopWatchTimer.ElapsedMilliseconds(startTime)}");
+
+var logger = new TraceLogger(new ConsoleLogger());
+logger.AddTraceStep("step1");
+Thread.Sleep(997);
+logger.AddTraceStep("step2");
+Thread.Sleep(997);
+logger.AddTraceStep("step3");
+Thread.Sleep(997);
+logger.AddTraceStep("step4");
+logger.LogTraceInfo("TestComponent", "TestEvent", new KeyValuePair<string, string>("param1", "value1"), new KeyValuePair<string, string>("param2", "value2"));
