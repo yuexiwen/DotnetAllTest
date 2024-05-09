8bb616b47702df869b6fb1e7e3b3512d5b406c2a
Bobby bob <bob@gmail.com>
2024-05-09 10:06:45 +0800

    2024-05-09 add demical test project

---
+++ DotnetAllTest/DemicalTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+double value = 12345.67891;
+var cultureLst = CultureInfo.GetCultures(CultureTypes.AllCultures);
+foreach (var item in cultureLst)
+{
+    Console.WriteLine($"{item.Name}");
+}
