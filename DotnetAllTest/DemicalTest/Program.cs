592659afb17044522d98282411d36845ac26b0c2
Bobby bob <bob@gmail.com>
2024-05-10 09:35:22 +0800

    2024-05-10 add demical test

--- DotnetAllTest/DemicalTest/Program.cs
+++ DotnetAllTest/DemicalTest/Program.cs
@@ -1,8 +1,30 @@
 ﻿using System.Globalization;
+using System.Text;
 
 double value = 12345.67891;
 var cultureLst = CultureInfo.GetCultures(CultureTypes.AllCultures);
-foreach (var item in cultureLst)
+
+Dictionary<string, List<string>> ValueCultureDict = new Dictionary<string, List<string>>();
+
+foreach (var culture in cultureLst)
 {
-    Console.WriteLine($"{item.Name}");
+    string key = value.ToString(culture);
+    if (!ValueCultureDict.ContainsKey(key))
+    {
+        ValueCultureDict[key] = new List<string>();
+    }
+
+    ValueCultureDict[key].Add(culture.Name);
+}
+
+
+foreach (var culturePair in ValueCultureDict)
+{
+    string cultures = string.Join(", ", culturePair.Value);
+    byte[] ucBytes = Encoding.Unicode.GetBytes(culturePair.Key);
+    for (int i = 0; i < ucBytes.Length; i++)
+    {
+        Console.Write($"{ucBytes[i]} ");
+    }
+    Console.WriteLine($"   key  = {culturePair.Key} | value = {cultures}\n\n");
 }
